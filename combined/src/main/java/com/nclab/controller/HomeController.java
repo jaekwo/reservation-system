ec3c1ce29c116cebad4c8b75d31eb612dc9ae007
inyu <inyu0603@gmail.com>
2023-04-12 16:23:35 +0900

    테스트

--- combined/src/main/java/com/nclab/controller/HomeController.java
+++ combined/src/main/java/com/nclab/controller/HomeController.java
@@ -1,10 +1,12 @@
 package com.nclab.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,10 +14,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.nclab.account.service.AccountServiceImpl;
+import com.nclab.account.vo.AccountVO;
+
 @Controller
 public class HomeController {
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	public AccountServiceImpl accountService;
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView home() {
@@ -34,7 +42,12 @@ public class HomeController {
 		return map;
 	}
 	
-	
-	
-	
+	@RequestMapping(value="/data2", method=RequestMethod.GET)
+	@ResponseBody
+	public Map<String, Object> data2() throws Exception {
+		Map<String, Object> rtnObj = new HashMap<String, Object>();
+		List<AccountVO> accountList = accountService.listAccount();
+		rtnObj.put("account_list", accountList);
+		return rtnObj;
+	}
 }

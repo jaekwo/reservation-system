b614b2a0c03e932d10bc60fa12d0cbf72431834c
우재권 <sunmina23@gmail.com>
2023-04-26 17:52:49 +0900

    axios

--- combined/frontend/src/App.vue
+++ combined/frontend/src/App.vue
@@ -4,39 +4,70 @@
     <div id="nav">
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
+      <MyHomeComponent></MyHomeComponent>
+      <h1>{{ title}}</h1>
+      <p>{{ count }}</p>
+      <button @click="count ++">추가</button>
+      <button @click="test">테스트</button>
+      <Mystatus></Mystatus>
     </div>
-    <router-view/>
+  
   </div>
 </template>
 
 <script>
-const axios = require('axios');
+import MyHomeComponent from "./HomeComponent.vue"
+import Mystatus from './StatusComponent'
+import axios from "axios"
+
+
 
 export default {
+ 
+  components : {
+    MyHomeComponent,
+    Mystatus
+  },
   data : function() {
     return {
-      message : ""
+      message : "",
+      title: "안녕하세요",
+      count: 1
     }
   },
   methods : {
-    load: function() {
-      axios.get("/data", {
-        params: {
-          msg: "Index..."
-        },
-        timeout: 1000
+    test(){
+      axios
+      .get("/")
+      .then(res => {
+        console.log(res)
       })
-      .then( res => {
-        // console.log(res.data)
-        this.message = res.data.message;
+      .catch(err =>{
+        console.log(err)
       })
-      .catch(
-       
-      );
+      .then(() =>{
+
+      })
     }
+    // load: function() {
+    //   axios.get("/data", {
+    //     params: {
+    //       msg: "Index..."
+    //     },
+    //     timeout: 1000
+    //   })
+    //   .then( res => {
+    //     // console.log(res.data)
+    //     this.message = res.data.message;
+    //   })
+    //   .catch(
+       
+    //   );
+    // }
   },
   mounted : function() {
     this.load();
   }
+
 }
 </script>
--- combined/frontend/src/main.js
+++ combined/frontend/src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import axios from 'axios';
+createApp(App).mount('#app');
 
-createApp(App).mount('#app')
+createApp.prototype.$axios = axios
